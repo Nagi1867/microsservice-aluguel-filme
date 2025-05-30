82cf203dd4f4bf4eee69fe46733e2194a4a356eb
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-05-30 18:47:47 -0300

    implementando o feign

--- api-filmes/src/main/java/com/ms/api_filmes/controller/FilmeController.java
+++ api-filmes/src/main/java/com/ms/api_filmes/controller/FilmeController.java
@@ -1,6 +1,7 @@
 package com.ms.api_filmes.controller;
 
 import com.ms.api_filmes.model.Filme;
+import com.ms.api_filmes.proxy.CambioProxy;
 import com.ms.api_filmes.repository.FilmeRepository;
 import com.ms.api_filmes.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
 import java.util.HashMap;
 
 @RestController
@@ -21,25 +21,40 @@ public class FilmeController {
     private Environment environment;
     @Autowired
     private FilmeRepository repository;
+    @Autowired
+    private CambioProxy proxy;
 
     @GetMapping(value = "/{id}/{currency}")
     public Filme findFilm(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
         var filme = repository.getReferenceById(id);
         if (filme == null) throw new RuntimeException("Film not found");
 
-        HashMap<String, String> params = new HashMap<>();
-        params.put("amount", filme.getPrice().toString());
-        params.put("from", "USD");
-        params.put("to", currency);
-
-
-        var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
-                Cambio.class, params);
-        var cambio = response.getBody();
+        var cambio = proxy.getCambio(filme.getPrice(), "USD", currency);
 
         var port = environment.getProperty("local.server.port");
         filme.setEnvironment(port);
         filme.setPrice(cambio.getConvertedValue());
         return filme;
     }
+
+//    @GetMapping(value = "/{id}/{currency}")
+//    public Filme findFilm(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
+//        var filme = repository.getReferenceById(id);
+//        if (filme == null) throw new RuntimeException("Film not found");
+//
+//        HashMap<String, String> params = new HashMap<>();
+//        params.put("amount", filme.getPrice().toString());
+//        params.put("from", "USD");
+//        params.put("to", currency);
+//
+//
+//        var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+//                Cambio.class, params);
+//        var cambio = response.getBody();
+//
+//        var port = environment.getProperty("local.server.port");
+//        filme.setEnvironment(port);
+//        filme.setPrice(cambio.getConvertedValue());
+//        return filme;
+//    }
 }
