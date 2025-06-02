fab8de71288ad507bd7ac9413eed386b39f11d22
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-02 19:39:42 -0300

    implementando retry com resilience4j api-filmes

--- api-filmes/src/main/java/com/ms/api_filmes/controller/FooBarController.java
+++ api-filmes/src/main/java/com/ms/api_filmes/controller/FooBarController.java
@@ -1,14 +1,23 @@
 package com.ms.api_filmes.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("api-filmes")
 public class FooBarController {
+    private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
     @GetMapping("/foo-bar")
+    @Retry(name = "foo-bar")
     public String fooBar() {
-        return "Foo-Bar!!";
+        logger.info("Request to foo-bar is received!");
+        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        return response.getBody();
     }
 }
