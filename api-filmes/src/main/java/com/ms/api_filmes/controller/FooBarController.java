c456248da55962d3136fe013955079a17662ed89
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-02 21:00:11 -0300

    rate limiter e bulhead

--- api-filmes/src/main/java/com/ms/api_filmes/controller/FooBarController.java
+++ api-filmes/src/main/java/com/ms/api_filmes/controller/FooBarController.java
@@ -1,7 +1,7 @@
 package com.ms.api_filmes.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +15,13 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    //@RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar() {
         logger.info("Request to foo-bar is received!");
-        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-        return response.getBody();
+        //var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        //return response.getBody();
+        return "foo-bar";
     }
 
     public String fallbackMethod(Exception ex) {
