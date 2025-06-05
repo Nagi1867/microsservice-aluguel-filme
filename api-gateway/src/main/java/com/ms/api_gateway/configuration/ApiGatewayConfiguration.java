d4543ae9ad73290d6306abb8947b4653c9c952e0
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-05 14:19:21 -0300

    dockerizando o naming-server

--- api-gateway/src/main/java/com/ms/api_gateway/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/ms/api_gateway/configuration/ApiGatewayConfiguration.java
@@ -1,5 +1,6 @@
 package com.ms.api_gateway.configuration;
 
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
--- api-gateway/src/main/java/com/ms/api_gateway/configuration/OpenApiConfiguration.java
+++ api-gateway/src/main/java/com/ms/api_gateway/configuration/OpenApiConfiguration.java
@@ -3,18 +3,23 @@ package com.ms.api_gateway.configuration;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
-    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config, RouteDefinitionLocator locator) {
+    @Bean
+    @Lazy(value = false)
+    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config,RouteDefinitionLocator locator) {
         var definitions = locator.getRouteDefinitions().collectList().block();
-        definitions.stream().filter(
-                routeDefinition -> routeDefinition.getId()
-                        .matches(".*-service"))
+
+        definitions.stream()
+                .filter(routeDefinition -> routeDefinition.getId()
+                        .matches(".*(-service|-filmes)"))
                 .forEach(routeDefinition -> {
                     String name = routeDefinition.getId();
                     config.addGroup(name);
@@ -22,6 +27,7 @@ public class OpenApiConfiguration {
                             .pathsToMatch("/"+name+"/**")
                             .group(name).build();
                 });
+
         return new ArrayList<>();
     }
 }
